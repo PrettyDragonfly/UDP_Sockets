752eb2a085bcb67bb760426aed47a2434dc56c1b
Camille1905 <bourcamille00@gmail.com>
2021-11-12 11:19:55 +0100

    try question 2

--- serveur_UDP.py
+++ serveur_UDP.py
@@ -1,7 +1,22 @@
 from socket import *
+import select
 
-mysocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-mysocket.bind(('', 2000))
-(message, adresse_client) = mysocket.recvfrom(60)
-mysocket.sendto(message, adresse_client)
-mysocket.close()
+mysocket1 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+mysocket2 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+
+mysocket1.bind(('', 2000))
+mysocket2.bind(('', 2002))
+
+sockets = [mysocket1, mysocket2]
+while True:
+    readable, writable, errored = select.select(sockets, [], [])
+
+    if mysocket1 in readable:
+        (message, client_address) = mysocket1.recvfrom(100)
+        mysocket1.sendto(message, (client_address[0], client_address[1] + 1))
+    else:
+        (message, client_address) = mysocket2.recvfrom(100)
+        mysocket2.sendto(message, (client_address[0], client_address[1] + 1))
+
+mysocket1.close()
+mysocket2.close()

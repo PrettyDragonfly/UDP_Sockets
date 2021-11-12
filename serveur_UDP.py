932702fcb644aa453320640385a4cf452c959940
Camille1905 <bourcamille00@gmail.com>
2021-11-12 11:23:51 +0100

    increase the octets' number

--- serveur_UDP.py
+++ serveur_UDP.py
@@ -4,7 +4,7 @@ mysocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
 
 mysocket.bind(('', 2000))
 
-(message, client_address) = mysocket.recvfrom(60)
+(message, client_address) = mysocket.recvfrom(100)
 
 mysocket.sendto(message, client_address)
 

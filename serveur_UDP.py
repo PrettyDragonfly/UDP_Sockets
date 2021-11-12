633c3846c261e17b90f6a54753f04193d2aec32e
Camille1905 <bourcamille00@gmail.com>
2021-11-12 11:22:18 +0100

    rewrite

--- serveur_UDP.py
+++ serveur_UDP.py
@@ -1,7 +1,11 @@
 from socket import *
 
 mysocket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+
 mysocket.bind(('', 2000))
-(message, adresse_client) = mysocket.recvfrom(60)
-mysocket.sendto(message, adresse_client)
+
+(message, client_address) = mysocket.recvfrom(60)
+
+mysocket.sendto(message, client_address)
+
 mysocket.close()

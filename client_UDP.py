5b4e312898e1f989b527e1cba70b9277eca8ad2e
Camille1905 <bourcamille00@gmail.com>
2021-11-14 14:43:45 +0100

    question_6 add exceptions

--- client_UDP.py
+++ client_UDP.py
@@ -1,17 +1,28 @@
 from socket import *
 import time
+import sys
 
 sockets = {}
 
 while True:
     for i in range(2000,2021,2):
-        sockets[i] = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-        sent = sockets[i].sendto(b"Hi !",
-                               ("", i))
-        sockets[i].bind(('', i+1))
-        (resultat, adresse_serveur) = sockets[i].recvfrom(100)
-        print("Le serveur a renvoyé : ",resultat," sur le port ",i+1)
-        time.sleep(1)
+        try:
+            sockets[i] = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+        except : sys.exit(1)
+
+        try:
+            sent = sockets[i].sendto(b"Hi !",
+                                   ("", i))
+        except:
+            print("Erreur lors de l'envoie du message sur le port ",i)
+
+        try:
+            sockets[i].bind(('', i+1))
+            (resultat, adresse_serveur) = sockets[i].recvfrom(100)
+            print("Le serveur a renvoyé : ",resultat," sur le port ",i+1)
+            time.sleep(1)
+        except:
+            print("Erreur lors de la réception du message sur le port ",i+1)
 
 for i in range(2000,2021,2):
     sockets[i].close()
--- serveur_UDP.py
+++ serveur_UDP.py
@@ -1,24 +1,37 @@
 from socket import *
 import select
+import sys
 
 sockets = {}
 
 for i in range(2000,2021,2):
-    print(i)
-    sockets[i] = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-    sockets[i].bind(('', i))
-    print(sockets[i])
+    try:
+        sockets[i] = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+        sockets[i].bind(('', i))
+    except:
+        sys.exit(1)
 
 while True:
     readable, writable, errored = select.select(sockets.values(), [], [])
 
     for sock in sockets.values():
         if sock in readable:
-            (message, client_address) = sock.recvfrom(100)
-            add, port = sock.getsockname()
-            sock.sendto(message, (client_address[0], port+1))
-            print("J'ai reçu :", message, " sur le port ", port,
+            try:
+                (message, client_address) = sock.recvfrom(100)
+                add, port = sock.getsockname()
+                sock.sendto(message, (client_address[0], port+1))
+                print("J'ai reçu :", message, " sur le port ", port,
                   " et je l'ai renvoyé sur le port ", port+1)
+            except:
+                print("Erreur lors de la réception du message")
+                break
+
+            try:
+                sock.sendto(message, (client_address[0], port + 1))
+                print("J'ai reçu :", message, " sur le port ", port,
+                      " et je l'ai renvoyé sur le port ", port + 1)
+            except:
+                print("Erreur lors de l'envoie du message sur le port ",port+1)
 
 for i in range(2000,2003,2):
     sockets[i].close()

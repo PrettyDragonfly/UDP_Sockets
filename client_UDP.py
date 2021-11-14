6536ffb140df1cb818c48ee885540ae9eaa69fcb
Camille1905 <bourcamille00@gmail.com>
2021-11-14 13:34:23 +0100

    add infinite loop in client

--- client_UDP.py
+++ client_UDP.py
@@ -1,14 +1,17 @@
 from socket import *
+import time
 
 sockets = {}
 
-for i in range(2000,2021,2):
-    sockets[i] = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
-    sent = sockets[i].sendto(b"Hi !",
-                           ("", i))
-    sockets[i].bind(('', i+1))
-    (resultat, adresse_serveur) = sockets[i].recvfrom(100)
-    print("Le serveur a renvoyé : ",resultat," sur le port ",i+1)
+while True:
+    for i in range(2000,2021,2):
+        sockets[i] = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+        sent = sockets[i].sendto(b"Hi !",
+                               ("", i))
+        sockets[i].bind(('', i+1))
+        (resultat, adresse_serveur) = sockets[i].recvfrom(100)
+        print("Le serveur a renvoyé : ",resultat," sur le port ",i+1)
+        time.sleep(1)
 
 for i in range(2000,2021,2):
     sockets[i].close()

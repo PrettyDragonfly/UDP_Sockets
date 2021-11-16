10bf3d55be4e2eb917f380f06aed8061b5670598
Camille1905 <bourcamille00@gmail.com>
2021-11-16 12:36:32 +0100

    add comments

--- serveur_UDP.py
+++ serveur_UDP.py
@@ -4,6 +4,7 @@ import sys
 
 sockets = {}
 
+# Création des sockets sur les ports 2000, 2002, ..., 2020
 for i in range(2000,2021,2):
     try:
         sockets[i] = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
@@ -16,6 +17,7 @@ while True:
 
     for sock in sockets.values():
         if sock in readable:
+            # Récupération du message
             try:
                 (message, client_address) = sock.recvfrom(100)
                 add, port = sock.getsockname()
@@ -26,6 +28,7 @@ while True:
                 print("Erreur lors de la réception du message")
                 continue
 
+            # Renvoie du message
             try:
                 sock.sendto(message, (client_address[0], port + 1))
                 print("J'ai reçu :", message, " sur le port ", port,
@@ -33,5 +36,6 @@ while True:
             except:
                 print("Erreur lors de l'envoie du message sur le port ",port+1)
 
+# Fermeture de toutes les sockets ouvertes
 for i in range(2000,2003,2):
     sockets[i].close()

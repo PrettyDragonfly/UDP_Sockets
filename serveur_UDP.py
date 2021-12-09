c6414a0ec009868ef39c6894f61283793e076054
Camille1905 <bourcamille00@gmail.com>
2021-12-09 14:09:22 +0100

    refactoring code

--- serveur_UDP.py
+++ serveur_UDP.py
@@ -2,10 +2,10 @@ from socket import *
 import select
 import sys
 
-sockets = {}
+sockets = dict()
 
 # Création des sockets sur les ports 2000, 2002, ..., 2020
-for i in range(2000,2021,2):
+for i in range(2000, 2021, 2):
     try:
         sockets[i] = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
         sockets[i].bind(('', i))
@@ -15,23 +15,21 @@ for i in range(2000,2021,2):
 while True:
     readable, writable, errored = select.select(sockets.values(), [], [])
 
-    for sock in sockets.values():
+    for port, sock in sockets.items():
         if sock in readable:
             # Récupération du message
             try:
                 (message, client_address) = sock.recvfrom(100)
-                add, port = sock.getsockname()
+                try:
+                    # Renvoie du message
+                    # add, port = sock.getsockname()
+                    sock.sendto(message, (client_address[0], port + 1))
+                    print("J'ai reçu :", message, " sur le port ", port,
+                          " et je l'ai renvoyé sur le port ", port + 1)
+                except:
+                    print("Erreur lors de l'envoie du message sur le port ", port + 1)
             except:
                 print("Erreur lors de la réception du message")
-                continue
-
-            # Renvoie du message
-            try:
-                sock.sendto(message, (client_address[0], port + 1))
-                print("J'ai reçu :", message, " sur le port ", port,
-                      " et je l'ai renvoyé sur le port ", port + 1)
-            except:
-                print("Erreur lors de l'envoie du message sur le port ",port+1)
 
 # Fermeture de toutes les sockets ouvertes
 for i in range(2000,2003,2):
